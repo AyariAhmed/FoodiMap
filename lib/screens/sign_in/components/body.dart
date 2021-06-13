74e3ce082c1b5107a783b6e767ac8763e4bfc78b
AyariAhmed <ayari17@hotmail.fr>
2021-06-13 02:25:49 +0100

    login screen finished

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:foodimap_flutter/components/custom_suffix_icon.dart';
 import 'package:foodimap_flutter/components/default_button.dart';
 import 'package:foodimap_flutter/components/form_error.dart';
+import 'package:foodimap_flutter/constants.dart';
+import 'package:foodimap_flutter/screens/sign_in/components/sign_form.dart';
 import 'package:foodimap_flutter/size_config.dart';
 
 class Body extends StatelessWidget {
@@ -10,112 +12,52 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: SingleChildScrollView(
       child: SizedBox(
         width: double.infinity,
         child: Padding(
           padding:
               EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: Column(
-            children: [
-              Text(
-                'Bon Retour Chez Nous!',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: getProportionateScreenWidth(25),
-                    fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 4),
-                child: Text(
-                  "Identifiez Vous avec votre Email/Num de telephone et votre mot de passe",
-                  textAlign: TextAlign.center,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  'Bon Retour Chez Nous!',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: getProportionateScreenWidth(25),
+                      fontWeight: FontWeight.bold),
                 ),
-              ),
-              SignForm()
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 4),
+                  child: Text(
+                    "Identifiez Vous avec votre Email/Num de telephone et votre mot de passe",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.05,),
+                SignForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.07,),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Vous n'avez pas de compte ?",
+                      style: TextStyle(
+                          fontSize: getProportionateScreenWidth(16)),
+                    ),Text(
+                      "Créer un compte",
+                      style: TextStyle(
+                          fontSize: getProportionateScreenWidth(16),
+                          color: kPrimaryColor
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    ));
-  }
-}
-
-class SignForm extends StatefulWidget {
-  const SignForm({Key key}) : super(key: key);
-
-  @override
-  _SignFormState createState() => _SignFormState();
-}
-
-class _SignFormState extends State<SignForm> {
-  final _formKey = GlobalKey<FormState>();
-  final List<String> errors = ["Demo Error"];
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          buildEmailFormField(),
-          SizedBox(
-            height: getProportionateScreenHeight(20),
-          ),
-          buildPasswordFormField(),
-          SizedBox(
-            height: getProportionateScreenHeight(20),
-          ),
-          FormError(errors: errors),
-          DefaultButton(
-              text: "Continuer",
-              press: () {
-                if (_formKey.currentState.validate()) {
-                  _formKey.currentState.save();
-                }
-              }),
-        ],
-      ),
-    );
-  }
-
-  TextFormField buildEmailFormField() {
-    return TextFormField(
-      validator: (value) {
-        if (value.isEmpty &&
-            !errors.contains(
-                "Prière d'ajouter votre Email ou Numéro de Téléphone")) {
-          setState(() {
-            errors.add("Prière d'ajouter votre Email ou Numéro de Téléphone");
-          });
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-          labelText: "Email/Numéro de telephone",
-          hintText: "Email/Télephone",
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          suffixIcon: customSuffix(svgIcon: "assets/icons/Mail.svg")),
-    );
-  }
-
-  TextFormField buildPasswordFormField() {
-    return TextFormField(
-      obscureText: true,
-      validator: (value) {
-        if (value.isEmpty &&
-            !errors.contains("Prière d'ajouter votre Mot de passe")) {
-          setState(() {
-            errors.add("Prière d'ajouter votre Mot de passe");
-          });
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-          labelText: "Mot de passe",
-          hintText: "Mot de passe",
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          suffixIcon: customSuffix(svgIcon: "assets/icons/Lock.svg")),
+      )
     );
   }
 }
---
+++ lib/screens/sign_in/components/sign_form.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:foodimap_flutter/components/custom_suffix_icon.dart';
+import 'package:foodimap_flutter/components/default_button.dart';
+import 'package:foodimap_flutter/components/form_error.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class SignForm extends StatefulWidget {
+  const SignForm({Key key}) : super(key: key);
+
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = [];
+
+  String email_phone;
+  String password;
+  bool remember = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          buildEmailFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(32),
+          ),
+          buildPasswordFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(25),
+          ),
+
+          Row(
+            children: [
+              Checkbox(value: remember,activeColor: kPrimaryColor, onChanged: (value){
+                setState(() {
+                  remember = value;
+                });
+
+              }),
+              Text("Se souvenir de moi")
+            ],
+          ),
+          FormError(errors: errors),
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          DefaultButton(
+              text: "Continuer",
+              press: () {
+                if (_formKey.currentState.validate()) {
+                  _formKey.currentState.save();
+                }
+              }),
+        ],
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    const emailTelValidation = "Prière d'ajouter votre Email ou Numéro de Téléphone";
+    return TextFormField(
+      onSaved: (newValue) => email_phone= newValue,
+      onChanged: (value){
+        if(value.isNotEmpty && errors.contains(emailTelValidation)){
+          setState(() {
+            errors.remove(emailTelValidation);
+          });
+        }
+      },
+      validator: (value) {
+        if (value.isEmpty &&
+            !errors.contains(emailTelValidation)) {
+          setState(() {
+            errors.add(emailTelValidation);
+          });
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          labelText: "Email/Numéro de telephone",
+          hintText: "Email/Télephone",
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          suffixIcon: customSuffix(svgIcon: "assets/icons/Mail.svg")),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value){
+        if(value.isNotEmpty && errors.contains(kPassNullError)){
+          setState(() {
+            errors.remove(kPassNullError);
+          });
+        }else if (value.length>= 6 && errors.contains(kShortPassError)){
+          setState(() {
+            errors.remove(kShortPassError);
+          });
+        }
+      },
+      validator: (value) {
+        if (value.isEmpty &&
+            !errors.contains(kPassNullError)) {
+          setState(() {
+            errors.add(kPassNullError);
+          });
+        }else if (value.length <6 && !errors.contains(kShortPassError)){
+          setState(() {
+            errors.add(kShortPassError);
+          });
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          labelText: "Mot de passe",
+          hintText: "Mot de passe",
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          suffixIcon: customSuffix(svgIcon: "assets/icons/Lock.svg")),
+    );
+  }
+}
